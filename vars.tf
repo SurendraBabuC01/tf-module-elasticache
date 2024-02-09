1f1491c2a13d129ae7a8a3d2ad56a9e0f9c7d8c3
surendra Babu C <surendra9703802865@gmail.com>
2024-02-09 11:50:49 +0530

    terraform

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "name" {}
+variable "env" {}
+variable "subnet_ids" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "port_no" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "num_node_groups" {}
+variable "node_type" {}
+variable "replicas_per_node_group" {}
